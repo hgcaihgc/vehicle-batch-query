5bebb56d8e4b2b67d12a3c772c893d93813108b9
hgcaihgc <hgcsx@126.com>
2021-04-29 14:05:05 +0800

    modified 3 files

--- classes.py
+++ classes.py
@@ -43,15 +43,16 @@ class GetVehicleNos:
 
 class LoginAndSearch:
     """功能模块类：登录和搜索"""
-    def __init__(self, address, username, password, vehicleNos):        
+    def __init__(self, address, username, password, vehicleNos, plateColor):        
         self.address = address
         self.username = username
         self.password = password
         self.vehicleNos = vehicleNos
+        self.plateColor = plateColor
 
     def get_cookie(self):
         """模拟登陆，获取cookie"""
-        print("正在登陆")
+        print("开始登陆。")
         driver = webdriver.Chrome()  # 调用webdriver模块下的Chrome()类并赋值给变量driver        
         driver.implicitly_wait(5)  # 设置隐式等待时间5s        
         driver.get(self.address)  # 直接进入指定页面，自动跳转到登录页面        
@@ -64,37 +65,43 @@ class LoginAndSearch:
         cookies = driver.get_cookies()  # 获取新页面的cookie        
         cookie = 'JSESSIONID=' + cookies[0]['value']  # 合成可用的cookie        
         driver.quit()  # 退出浏览器
-        print("登陆完成")
+        print("登陆完成。")
         return cookie    
 
     def search_by_vehicleNos(self):
-        """通过所有车牌号查询信息"""
+        """通过所有车牌号查询信息"""        
         cookie = self.get_cookie()  # 获取cookie信息
+        print("开始查询。")
         vehicle_num = len(self.vehicleNos)  # 获取车牌号码列表的长度，即车辆的总数
         vehicle_new = self.vehicleNos[:]  # 复制车牌号列表
         contents = []  # 定义需要查询结果的空列表
         particulars = []  # 定义需要查询的详情的空列表
-        message_progress = "正在查询{0:>8s}，这是第{1:>2d}次查询,进程为{2:>4d}/{3:>4d}, 进度为{4:>6.2f}%。"  # 定义进程信息
+        message_progress = "正在查询{0:>8s}，这是该车第{1:>2d}次查询,进程为{2:>4d}/{3:>4d}, 进度为{4:>6.2f}%。"  # 定义进程信息
         i = 1
+        total_count = 1
         while vehicle_new:
             count = 1  # 尝试连接的次数，初始化为0
             vehicleNo = vehicle_new[-1]
             while True:  #
                 print('\r', message_progress.format(vehicleNo, count, i, vehicle_num, i*100/vehicle_num), end='', flush=True)  # 输出进程信息
                 try:
-                    content = search_by_vehicleNo(vehicleNo, cookie)   # 通过车牌号码查询                    
+                    content = search_by_vehicleNo(vehicleNo, cookie, self.plateColor)   # 通过车牌号码查询                    
                     content = simplify_content(content)  # 简化查询结果，主要是针对有读条记录的查询结果选择一个最新的
                     particular = get_particulars(content, cookie)  # 查询详情                    
                     contents.append(content)  # 将查询结果加入到结果列表
                     particulars.append(particular)  # 将详情结果加入到详情列表
                     i += 1
+                    total_count += 1
                     vehicle_new.pop()
                     break  # 跳出该车牌号的查询
                 except requests.exceptions.RequestException:  # 获取异常,查询异常时，如网速过慢超时
-                    count = count + 1  # 查询次数加1
+                    count += 1  # 查询次数加1
+                    total_count += 1
                     sleep(0.5)
                     if count > 99:
                         sys.exit("网络不畅，请稍后再试！")
+        print("\n", end='')
+        print("查询结束。本次查询{0:>4d}辆车，共查询{1:>4d}次，平均每车查询{2:>4.2f}次。".format(vehicle_num, total_count, total_count/vehicle_num))
         return [contents, particulars]  # 返回查询结果、详情列表，完成查询和未完成查询的列表
 
     def search_by_vehicleNos_with_nonoperating(self):
@@ -128,6 +135,7 @@ class OutPut:
 
     def output_new_xls(self):
         """将最后结果输出到xlsx文件"""
+        print("开始写入")
         workbook = xlwt.Workbook()        
         sheet = workbook.add_sheet('结果', cell_overwrite_ok=True)  # 添加name为'结果'的sheet        
         title = ['车牌号码', '证件号码', '有效期至', '营运状态',
@@ -185,9 +193,13 @@ class OutPut:
                 tr = [content["vehicleNo"], 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None']
             [sheet.write(i+1, j, tr[j]) for j in range(col_num)]
         workbook.save('结果-{0}.xls'.format(datetime.now().strftime('%Y%m%d-%H%M%S')))
+        print("\n", end='')
+        print("写入完成。")
+
 
     def output_old_xls(self, target_mark="预处理"):
-        """将最后结果输出到原xls文件，只有在只有一个xls文件时才适用"""        
+        """将最后结果输出到原xls文件，只有在只有一个xls文件时才适用"""
+        print("开始写入")
         title = ['证件号码', '有效期至', '营运状态', '经营范围',
                  '发证机构', '业户名称', '经营许可证号', '有效期至',
                  '营运状态', '经营范围', '发证机构']  # 自定义标题行
@@ -207,6 +219,8 @@ class OutPut:
         [copy_sheet.write(index[0], ncols+j, title[j]) for j in range(col_num)]
         contents_num = len(self.contents)
         for i in range(contents_num):
+            # 输出进程信息
+            print('\r', "正在写入，进程为{0:>4d}/{1:>4d}, 进度为{2:>6.2f}%".format(i+1, contents_num, (i+1)*100/contents_num), end='', flush=True)
             content = self.contents[i]
             particular = self.particulars[i]
             if content['total'] > 0:            
@@ -214,18 +228,32 @@ class OutPut:
                     # 在有certificateInfo信息的情况时
                     if 'certificateInfo' in content['rows'][count].keys():
                         if particular['total'] > 0:
-                            tr = [content['rows'][count]['certificateInfo'][0]['transCertificateCode'],
-                                  content['rows'][count]['certificateInfo'][0]['certificateExpireDate'],
-                                  content['rows'][count]['businessState'],
-                                  content['rows'][count]['certificateInfo'][0]['businessScopeDesc'],
-                                  content['rows'][count]['certificateInfo'][0]['transCertificateGrantOrgan'],
-                                  particular['rows'][0]['ownerName'],
-                                  particular['rows'][0]['certificateInfo'][0]['licenseCode'],
-                                  particular['rows'][0]['certificateInfo'][0]['expireDate'],
-                                  particular['rows'][0]['operatingStatus'],
-                                  particular['rows'][0]['certificateInfo'][0]['businessScopeDesc'],
-                                  particular['rows'][0]['certificateInfo'][0]['licenseIssueOrgan']
-                                  ]
+                            if 'certificateInfo' in particular['rows'][0].keys():  # 在particular有certificateInfo信息的情况时
+                                tr = [content['rows'][count]['certificateInfo'][0]['transCertificateCode'],
+                                    content['rows'][count]['certificateInfo'][0]['certificateExpireDate'],
+                                    content['rows'][count]['businessState'],
+                                    content['rows'][count]['certificateInfo'][0]['businessScopeDesc'],
+                                    content['rows'][count]['certificateInfo'][0]['transCertificateGrantOrgan'],
+                                    particular['rows'][0]['ownerName'],
+                                    particular['rows'][0]['certificateInfo'][0]['licenseCode'],
+                                    particular['rows'][0]['certificateInfo'][0]['expireDate'],
+                                    particular['rows'][0]['operatingStatus'],
+                                    particular['rows'][0]['certificateInfo'][0]['businessScopeDesc'],
+                                    particular['rows'][0]['certificateInfo'][0]['licenseIssueOrgan']
+                                    ]
+                            else:  # 在particular没有certificateInfo信息的情况时
+                                tr = [content['rows'][count]['vehicleNo'],
+                                      content['rows'][count]['certificateInfo'][0]['transCertificateCode'],
+                                      content['rows'][count]['certificateInfo'][0]['certificateExpireDate'],
+                                      content['rows'][count]['businessState'],
+                                      content['rows'][count]['certificateInfo'][0]['businessScopeDesc'],
+                                      content['rows'][count]['certificateInfo'][0]['transCertificateGrantOrgan'],
+                                      particular['rows'][0]['ownerName'],
+                                      'None',
+                                      'None',
+                                      particular['rows'][0]['operatingStatus'],
+                                      'None',
+                                      'None']
                         else:
                             tr = [content['rows'][count]['vehicleNo'],
                                   content['rows'][count]['certificateInfo'][0]['transCertificateCode'],
@@ -234,14 +262,12 @@ class OutPut:
                                   content['rows'][count]['certificateInfo'][0]['businessScopeDesc'],
                                   content['rows'][count]['certificateInfo'][0]['transCertificateGrantOrgan'],
                                   'None', 'None', 'None', 'None', 'None', 'None']
-                        [copy_sheet.write(index[0]+1+i, ncols+j, tr[j]) for j in range(col_num)]
                     # 在没有certificateInfo信息的情况时
                     else:
                         tr = ['None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None']
-                        [copy_sheet.write(index[0]+1+i, ncols+j, tr[j]) for j in range(col_num)]
             else:            
                 tr = ['None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None', 'None']
-                [copy_sheet.write(index[0]+1+i, ncols+j, tr[j]) for j in range(col_num)]
-            # 输出进程信息
-            print('\r', "正在写入，进程为{0:>3d}/{1:>3d}, 进度为{2:>6.2f}%".format(i + 1, contents_num, (i + 1) * 100 / contents_num), end='', flush=True)
+            [copy_sheet.write(index[0]+1+i, ncols+j, tr[j]) for j in range(col_num)]
         copy_work_book.save(file_name)
+        print("\n", end='')
+        print("写入完成。")
--- functions.py
+++ functions.py
@@ -86,11 +86,11 @@ def get_provinceCode(vehicleNo):
     return provinceCode
 
 
-def search_by_vehicleNo(vehicleNo, cookie):
+def search_by_vehicleNo(vehicleNo, cookie, plateColor):
     """通过一个车牌号查询信息"""
     url = 'http://10.100.32.31:8138/SQS/overProvince/getResDirectory'
     plateColor_list = ['所有', '蓝色', '黄色', '黑色', '白色', '黄绿色', '渐变绿', '其他']
-    plateColorCode = str(plateColor_list.index("黄色"))
+    plateColorCode = str(plateColor_list.index(plateColor))
     params_dict = {"vehicleNo": vehicleNo, "plateColorCode": plateColorCode, "provinceCode": get_provinceCode(vehicleNo)}
     From_data={
         'params': str(params_dict),
